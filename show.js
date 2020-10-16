830b04275e92b45ed64aa4ca99f33771430e7ade
Merhern <runuk305@seznam.cz>
2020-10-16 16:13:54 +0200

    fix show.js

--- show.js
+++ show.js
@@ -16,3 +16,75 @@ article.appendChild(popis);
 document.getElementById("nabidka").appendChild(article);
 
 //test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
