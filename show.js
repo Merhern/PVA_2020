46b525d3db11e91f628e3840f5c5a8f01a30a7d4
Merhern <runuk305@seznam.cz>
2020-10-16 16:59:33 +0200

    files

---
+++ show.js
@@ -0,0 +1,232 @@
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var obrazek = document.createElement("IMG");
+obrazek.setAttribute("src","McDonalds_Fries.png");
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(obrazek);
+article.appendChild(popis);
+
+document.getElementById("nabidka").appendChild(article);
