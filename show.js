b0c46f0473f9fb09a12860a2fb2732404802a99a
Merhern <runuk305@seznam.cz>
2020-10-16 15:55:03 +0200

    upload všeho na github

---
+++ show.js
@@ -0,0 +1,54 @@
+var article = document.createElement("ARTICLE");
+article.setAttribute("id","12345")
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+var obrazek = document.createElement("img");
+obrazek.innerHTML = "McDonalds_Fries.png";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(popis);
+article.appendChild(obrazek);
+
+document.getElementById("nabidka").appendChild(article);
+
+//test ---
+var article = document.createElement("ARTICLE");
+article.setAttribute("id","12345")
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+var obrazek = document.createElement("img");
+obrazek.innerHTML = "McDonalds_Fries.png";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(popis);
+article.appendChild(obrazek);
+
+document.getElementById("nabidka").appendChild(article);
+
+var article = document.createElement("ARTICLE");
+article.setAttribute("id","12345")
+var nazev = document.createElement("H2");
+nazev.innerHTML = "Test1";
+var cena = document.createElement("H4");
+cena.innerHTML = "200 Kč";
+var popis = document.createElement("P");
+popis.innerHTML = "test test test test test test test test test test test test test test test";
+var obrazek = document.createElement("img");
+obrazek.innerHTML = "McDonalds_Fries.png";
+
+article.appendChild(nazev);
+article.appendChild(cena);
+article.appendChild(popis);
+article.appendChild(obrazek);
+
+document.getElementById("nabidka").appendChild(article);
